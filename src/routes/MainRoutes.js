d2e727509727aff0eb4a6168a8ba2ab7da95e760
7ganam@gmail.com <7ganam@gmail.com>
2022-04-13 05:20:53 +0200

    cleand unsued files

--- src/routes/MainRoutes.js
+++ src/routes/MainRoutes.js
@@ -8,14 +8,7 @@ import Loadable from 'ui-component/Loadable';
 const DashboardDefault = Loadable(lazy(() => import('views/dashboard/Default')));
 
 // utilities routing
-const UtilsTypography = Loadable(lazy(() => import('views/utilities/Typography')));
-const UtilsColor = Loadable(lazy(() => import('views/utilities/Color')));
-const UtilsShadow = Loadable(lazy(() => import('views/utilities/Shadow')));
-const UtilsMaterialIcons = Loadable(lazy(() => import('views/utilities/MaterialIcons')));
 const UtilsStationPage = Loadable(lazy(() => import('views/utilities/StationPage')));
-
-// sample page routing
-const SamplePage = Loadable(lazy(() => import('views/sample-page')));
 
 // ==============================|| MAIN ROUTING ||============================== //
 
@@ -32,28 +25,8 @@ const MainRoutes = {
             element: <DashboardDefault />
         },
         {
-            path: '/utils/util-typography',
-            element: <UtilsTypography />
-        },
-        {
-            path: '/utils/util-color',
-            element: <UtilsColor />
-        },
-        {
-            path: '/utils/util-shadow',
-            element: <UtilsShadow />
-        },
-        {
             path: '/stations/:id',
             element: <UtilsStationPage />
-        },
-        {
-            path: '/icons/material-icons',
-            element: <UtilsMaterialIcons />
-        },
-        {
-            path: '/sample-page',
-            element: <SamplePage />
         }
     ]
 };
